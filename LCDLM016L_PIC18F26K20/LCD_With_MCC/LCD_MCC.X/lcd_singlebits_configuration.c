e0df02041fa1490fc6d33cbc226ac4b5a1d6185e
zachar <z.papkov@gmail.com>
2020-03-29 22:31:02 +0300

    add new project on mikroc  working  sequential

--- LCDLM016L_PIC18F26K20/LCD_With_MCC/LCD_MCC.X/lcd_singlebits_configuration.c
+++ LCDLM016L_PIC18F26K20/LCD_With_MCC/LCD_MCC.X/lcd_singlebits_configuration.c
@@ -1,1 +1,132 @@
+#include "lcd_singlebits_configuration.h"
+#include <xc.h>
+#include "mcc_generated_files/mcc.h"
+
+#define	LCD_RS PORTCbits.RC4
+#define LCD_EN PORTCbits.RC5
+
+#define LCD_D0 PORTBbits.RB5
+#define LCD_D1 PORTBbits.RB4
+#define LCD_D2 PORTBbits.RB3
+#define LCD_D3 PORTBbits.RB2
+
+#define LCD_DATA	PORTB
+
+#define	LCD_STROBE()	((LCD_EN = 1),(LCD_EN=0))
+
+typedef union
+{
+	uint8_t	ch;
+	struct
+	{
+		unsigned	b0:		1;
+		unsigned	b1:		1;
+		unsigned	b2:		1;
+		unsigned	b3:		1;
+		unsigned	b4:		1;
+		unsigned	b5:		1;
+		unsigned	b6:		1;
+		unsigned	b7:		1;
+	} bits;
+} lcdbyte;
+
+lcdbyte lcd_bits;
+
+/* write a byte to the LCD in 4 bit mode */
+
+void
+lcd_write(unsigned char c)
+{
+    lcd_bits.ch=c;
+
+	__delay_us(20);
+    LCD_D0 = lcd_bits.bits.b7;
+    LCD_D1 = lcd_bits.bits.b6;
+    LCD_D2 = lcd_bits.bits.b5;
+    LCD_D3 = lcd_bits.bits.b4;
+    LCD_STROBE();
+    //__delay_us(20);
+    LCD_D0 = lcd_bits.bits.b3;
+    LCD_D1 = lcd_bits.bits.b2;
+    LCD_D2 = lcd_bits.bits.b1;
+    LCD_D3 = lcd_bits.bits.b0;
+    LCD_STROBE();
+
+    
+}
+
+void
+lcd_clear(void)
+{
+	LCD_RS = 0;
+	lcd_write(0x1);
+	__delay_ms(2);
+}
+
+/* write a string of chars to the LCD */
+
+void
+lcd_puts(const char * s)
+{
+	LCD_RS = 1;	// write characters
+	while(*s)
+		lcd_write(*s++);
+}
+
+/* write one character to the LCD */
+
+void
+lcd_putch(char c)
+{
+	LCD_RS = 1;	// write characters
+	lcd_write( c );
+}
+
+
+/*
+ * Go to the specified position
+ */
+
+void
+lcd_goto(unsigned char pos)
+{
+	LCD_RS = 0;
+	lcd_write(0x80+pos);
+}
+	
+/* initialise the LCD - put into 4 bit mode */
+void
+lcd_init()
+{
+	char init_value;
+
+	init_value = 0x3;
+	LCD_RS = 0;
+	LCD_EN = 0;
+	//LCD_RW = 0;
+	
+	__delay_ms(15);	// wait 15mSec after power applied,
+	LCD_DATA	 = init_value;
+	LCD_STROBE();
+	__delay_ms(5);
+	LCD_STROBE();
+	__delay_us(200);
+	LCD_STROBE();
+	__delay_us(200);
+	LCD_DATA = 2;	// Four bit mode
+	LCD_STROBE();
+
+	lcd_write(0x28); // Set interface length
+    lcd_write(0x0C);  // Display On, Cursor Off
+	//lcd_write(0xF);  // Display On, Cursor On, Cursor Blink
+	lcd_clear();	// Clear screen
+	lcd_write(0x6); // Set entry Mode
+}
+
+void lcd_start(void)
+{
+    lcd_init();
+    lcd_clear();
+}
+
 
--- LCDLM016L_PIC18F26K20/LCD_With_MCC/LCD_MCC.X/lcd_singlebits_configuration.h
+++ LCDLM016L_PIC18F26K20/LCD_With_MCC/LCD_MCC.X/lcd_singlebits_configuration.h
@@ -1,31 +1,3 @@
-/* Microchip Technology Inc. and its subsidiaries.  You may use this software 
- * and any derivatives exclusively with Microchip products. 
- * 
- * THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS".  NO WARRANTIES, WHETHER 
- * EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED 
- * WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A 
- * PARTICULAR PURPOSE, OR ITS INTERACTION WITH MICROCHIP PRODUCTS, COMBINATION 
- * WITH ANY OTHER PRODUCTS, OR USE IN ANY APPLICATION. 
- *
- * IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
- * INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND 
- * WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS 
- * BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE.  TO THE 
- * FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS 
- * IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF 
- * ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
- *
- * MICROCHIP PROVIDES THIS SOFTWARE CONDITIONALLY UPON YOUR ACCEPTANCE OF THESE 
- * TERMS. 
- */
-
-/* 
- * File:   
- * Author: 
- * Comments:
- * Revision history: 
- */
-
 // This is a guard condition so that contents of this file are not included
 // more than once.  
 #ifndef XC_HEADER_TEMPLATE_H
@@ -62,6 +34,39 @@
  */
 // TODO Insert declarations or function prototypes (right here) to leverage 
 // live documentation
+
+#include <stdint.h>
+
+/* write a byte to the LCD in 4 bit mode */
+
+ void lcd_write(unsigned char);
+
+/* Clear and home the LCD */
+
+ void lcd_clear(void);
+
+/* write a string of characters to the LCD */
+
+ void lcd_puts(const char * s);
+
+/* Go to the specified position */
+
+ void lcd_goto(unsigned char pos);
+	
+/* intialize the LCD - call before anything else */
+
+ void lcd_init(void);
+
+void lcd_putch(char);
+
+/*	Set the cursor position */
+
+#define	lcd_cursor(x)	lcd_write(((x)&0x7F)|0x80)
+
+void lcd_put_float(float val ,uint8_t lcd_position);
+
+void lcd_start(void);
+
 
 #ifdef	__cplusplus
 extern "C" {
